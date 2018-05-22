a5e8678388494927d2d61313195bd5469fe2a284
sdhaygud <37179662+sdhaygud@users.noreply.github.com>
2018-05-22 17:10:48 +0530

    Update Second.java

--- JavaProject/src/Second.java
+++ JavaProject/src/Second.java
@@ -4,6 +4,6 @@ public class Second {
 	public static void main(String[] args) {
 		System.out.println("Frist commite");
 		System.out.println("second commite");
-		System.out.println("third commite master real change second another eclipse");
+		System.out.println("third commite master real change second another repo entry eclipse");
 	}
 }
