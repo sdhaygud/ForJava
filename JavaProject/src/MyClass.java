433edb6dadb4cfd37f909fa6da8190e168e7d534
sdhaygud <37179662+sdhaygud@users.noreply.github.com>
2018-05-22 17:01:50 +0530

    Update MyClass.java

--- JavaProject/src/MyClass.java
+++ JavaProject/src/MyClass.java
@@ -2,7 +2,7 @@
 public class MyClass {
 
 	public static void main(String[] args) {
-		System.out.println("first second master change myclass");
+		System.out.println("first second master change myclass repo change");
 	}
 	
 }
